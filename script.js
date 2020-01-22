50e1dde02fc6b5d88ff7ee570657afbcc0691c39
Tay811 <mataitaya@gmail.com>
2020-01-22 09:45:59 +0300

    second commit

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let block2 = document.getElementById("container2");
 let block3 = document.getElementById("container3");
 
 function changeStyle(){
-    maine.style.backgroundImage = "url('../img/1.jpg')";
+    maine.style.backgroundImage = "url('./img/1.jpg')";
     block1.style.backgroundColor = ("rgba(62, 129, 71, 0.514)");
     block2.style.backgroundColor = ("rgba(62, 129, 71, 0.514)");
     block3.style.backgroundColor = ("rgba(62, 129, 71, 0.514)");
@@ -19,15 +19,11 @@ function changeStyle(){
 let flexPage = document.getElementById("page");
 
 function newFlexBlock(){
-    // maine.style.display = "none";
-    // flexPage.style.display = "flex";
-    if(maine.style.display == "flex"){
- 		maine.style.display = "none";
- 		flexPage.style.display = "flex";
-    }
+    maine.style.display = "none";
+    flexPage.style.display = "flex";
 }
 
-newFlexBlock()
+
 
 
 
